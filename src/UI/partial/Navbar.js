31a7060af96cb454ffe09acffee8687a5f502c88
Abdulqawiyyi abiodun <86069758+horlash-Dev@users.noreply.github.com>
2022-11-03 23:47:46 +0100

    useEffect & useRducer update

--- src/UI/partial/Navbar.js
+++ src/UI/partial/Navbar.js
@@ -1,23 +1,47 @@
 import { Fragment} from "react";
 
-const Navbar = () => {
+const Navbar = (props) => {
+const home = (event) => {
+    event.preventDefault();
+    props.homeView(false)
+}
 
+const login = (event) => {
+    event.preventDefault();
+    props.loginView(true)
+}
+
+const userProfile = (event) => {
+    event.preventDefault();
+    props.profileView(true)
+}
+
+const logout = (event) => {
+    event.preventDefault();
+    props.logOUt(false)
+}
+const auth = props.isAuth
     return(
     <Fragment>
 
    <ul className="nav nav-pills nav-stacked">
     <li className="nav-item">
-        <a href="#a" className="nav-link active">home</a>
+        <a href="#a" className="nav-link active" onClick={home}>home</a>
     </li>
-    <li className="nav-item">
-        <a href="#a" className="nav-link">dashboard</a>
-    </li>
-    <li className="nav-item">
-        <a href="#a" className="nav-link">login</a>
-    </li>
-    <li className="nav-item">
-        <a href="#a" className="nav-link">logout</a>
-    </li>
+    { auth &&
+    (<li className="nav-item">
+        <a href="#a" className="nav-link" onClick={userProfile}>dashboard</a>
+    </li>)
+     }
+     { !auth ?
+       
+    (<li className="nav-item">
+        <a href="#a" className="nav-link" onClick={login}>login</a>
+    </li>) :
+   (<li className="nav-item">
+        <a href="#a" className="nav-link" onClick={logout}>logout</a>
+    </li> )
+     }
 </ul>
         </Fragment>
     )
--- src/component/dashboard/Dashboard.js
+++ src/component/dashboard/Dashboard.js
@@ -1,10 +1,11 @@
 
 import Card from "../../UI/Card"
 const Dashboard = (props) => {
+const getUser = localStorage.getItem('auth_user')
 return (
 <Card className="border-dark bg-light text-dark text-center border-2" >
 <div className="card-body">
-<h4 className="card-title fs-1 text-uppercase mb-4">welcome back</h4>
+<h4 className="card-title fs-1 text-uppercase mb-4">welcome back  <i>{ getUser } </i> </h4>
     <p className="card-text fs-2 text-capitalize">signed at time</p>
   </div>
 </Card>
